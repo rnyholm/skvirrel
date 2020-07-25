76c738d50ee5f172b3ec7781549172acbeae3fd1
Robert Nyholm <ronyholm@gmail.com>
2020-07-25 21:26:51 +0300

    #13 #12 Progress on user interface, switched over to constraint layout, some keyboard handling

--- app/src/main/java/ax/stardust/skvirrel/activity/Skvirrel.java
+++ app/src/main/java/ax/stardust/skvirrel/activity/Skvirrel.java
@@ -1,16 +1,47 @@
 package ax.stardust.skvirrel.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import ax.stardust.skvirrel.R;
+import ax.stardust.skvirrel.component.keyboard.KeyboardHandler;
+import ax.stardust.skvirrel.component.keyboard.SkvirrelKeyboard;
+import ax.stardust.skvirrel.component.widget.KeyboardlessEditText;
 
 public class Skvirrel extends AppCompatActivity {
+    private SkvirrelKeyboard skvirrelKeyboard;
+
+    private TextView versionNameTextView;
+
+    private KeyboardlessEditText tickerEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skvirrel);
+        findViews();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (skvirrelKeyboard.getVisibility() == View.VISIBLE) {
+            // hacky way to release focus from any edit text, by releasing it also the keyboard will be closed
+            versionNameTextView.requestFocus();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void findViews() {
+        skvirrelKeyboard = findViewById(R.id.soft_keyboard);
+
+        versionNameTextView = findViewById(R.id.version_name_tv);
+
+        tickerEditText = findViewById(R.id.ticker_et);
+        tickerEditText.setOnFocusChangeListener(new KeyboardHandler(skvirrelKeyboard));
+        tickerEditText.setOnTouchListener(new KeyboardHandler(skvirrelKeyboard));
     }
 }
