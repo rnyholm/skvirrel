54913e280591c3684a8f5e70d98365f9c831dd40
Robert Nyholm <ronyholm@gmail.com>
2021-01-31 19:38:48 +0200

    #32 Times for scheduled job are resolved based on build config

--- app/src/main/java/ax/stardust/skvirrel/schedule/MonitoringScheduler.java
+++ app/src/main/java/ax/stardust/skvirrel/schedule/MonitoringScheduler.java
@@ -5,6 +5,7 @@ import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
 
+import ax.stardust.skvirrel.BuildConfig;
 import ax.stardust.skvirrel.service.MonitoringJobService;
 import ax.stardust.skvirrel.service.ServiceParams;
 import timber.log.Timber;
@@ -15,8 +16,10 @@ import timber.log.Timber;
 public class MonitoringScheduler {
 
     // preferred and maximum time between runs in seconds
-    private static final int PREFERRED_POLL_TIME = 10; // 900 in production (15min)
-    private static final int MAXIMUM_POLL_TIME = 40; // 1200 in production (20min)
+    private static final int PREFERRED_POLL_TIME_DEBUG = 60;    // 1 minute
+    private static final int MAXIMUM_POLL_TIME_DEBUG = 120;     // 2 minutes
+    private static final int PREFERRED_POLL_TIME_RELEASE = 900; // 15 minutes
+    private static final int MAXIMUM_POLL_TIME_RELEASE = 1200;  // 20 minutes
 
     /**
      * Schedules monitoring jobs
@@ -26,8 +29,8 @@ public class MonitoringScheduler {
     public static void scheduleJob(Context context) {
         ComponentName serviceComponent = new ComponentName(context, MonitoringJobService.class);
         JobInfo.Builder builder = new JobInfo.Builder(ServiceParams.MONITORING_JOB_SERVICE_ID, serviceComponent);
-        builder.setMinimumLatency(PREFERRED_POLL_TIME * 1000);
-        builder.setOverrideDeadline(MAXIMUM_POLL_TIME * 1000);
+        builder.setMinimumLatency(resolveMinimumLatency());
+        builder.setOverrideDeadline(resolveOverrideDeadline());
         builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
         builder.setRequiresCharging(false);
         builder.setPersisted(true);
@@ -36,5 +39,25 @@ public class MonitoringScheduler {
         jobScheduler.schedule(builder.build());
 
         Timber.d("Monitoring job successfully scheduled");
+    }
+
+    /**
+     * Resolves minimum latency to be used for scheduled job. Different times are calculated
+     * depending on if it's a debug or release build. Debug builds run much more frequent.
+     *
+     * @return resolved minimum latency
+     */
+    private static long resolveMinimumLatency() {
+        return 1000 * (BuildConfig.DEBUG ? PREFERRED_POLL_TIME_DEBUG : PREFERRED_POLL_TIME_RELEASE);
+    }
+
+    /**
+     * Resolves override deadline to be used for scheduled job. Different times are calculated
+     * depending on if it's a debug or release build. Debug builds run much more frequent.
+     *
+     * @return resolved override deadline
+     */
+    private static long resolveOverrideDeadline() {
+        return 1000 * (BuildConfig.DEBUG ? MAXIMUM_POLL_TIME_DEBUG : MAXIMUM_POLL_TIME_RELEASE);
     }
 }
